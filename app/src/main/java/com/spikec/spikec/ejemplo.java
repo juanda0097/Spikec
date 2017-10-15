41e8c216f6cbca50b341b220cd1bdc2a37b147d3
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-10-14 23:37:21 -0500

    quinto commit se le agregaron los botones de fechas y horas funcionando

--- app/src/main/java/com/spikec/spikec/ejemplo.java
+++ app/src/main/java/com/spikec/spikec/ejemplo.java
@@ -1,20 +1,31 @@
 package com.spikec.spikec;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class ejemplo extends AppCompatActivity {
+    TextView txtfecha;
+    TextView txthora;
+    Button btnfecha;
+    Button btnhora;
     Button bano;
     TextView tv;
+    //variables globales
+    private int year,mes,dia,horas,minutos;
     public boolean[] checkedColors = new boolean[]{
             false, // Red
             false, // Green
@@ -30,6 +41,67 @@ public class ejemplo extends AppCompatActivity {
         setContentView(R.layout.activity_ejemplo);
         bano = (Button)findViewById(R.id.btnbano);
         tv = (TextView)findViewById(R.id.txtselec);
+
+        //identificar la vista de fecha y hora
+
+        txtfecha=(TextView)findViewById(R.id.txtfecha);
+        txthora=(TextView)findViewById(R.id.txthora);
+
+        //identificar los botones
+
+        btnfecha=(Button)findViewById(R.id.btnfecha);
+        btnhora=(Button)findViewById(R.id.btnhora);
+
+        //colocar el bton poner fecha a la espera de un clic
+
+        btnfecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //obtiene la fecha actual
+                final Calendar c = Calendar.getInstance();
+                // se captura eñ año mes y dia de la fecha actual en tres variables
+                year=c.get(Calendar.YEAR);
+                mes=c.get(Calendar.MONTH);
+                dia=c.get(Calendar.DAY_OF_MONTH);
+
+                // creamos el dialogdatatepiker
+                DatePickerDialog datePicker = new DatePickerDialog(ejemplo.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        txtfecha.setText(year+"/"+month+"/"+dayOfMonth);
+                    }
+                },year,mes,dia);
+                // se muestra el cuadro de dialogo
+                datePicker.show();
+            }
+        });
+
+        // colocamos el otro boton (hora) a la espera
+
+        btnhora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // en este se establece la hora actual a diferencia del anterior
+
+                final Calendar c = Calendar.getInstance();
+                horas=c.get(Calendar.HOUR_OF_DAY);
+                minutos=c.get(Calendar.MINUTE);
+
+                // creamos el DialogTimePicker
+                TimePickerDialog ponerhora =new TimePickerDialog(ejemplo.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        txthora.setText(hourOfDay+":"+minute);
+
+                    }
+                },horas,minutos,false);
+
+                // mostramos el elemento timer como una ventana de dialogo
+                ponerhora.show();
+            }
+        });
+
         bano.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
