5af6c035d59a91b1a3aa84c3ad1e164a17ce7d9d
juanda0097 <31812666+juanda0097@users.noreply.github.com>
2017-10-18 12:56:31 -0500

    9.2 registro funcionando

--- app/src/main/java/com/spikec/spikec/login.java
+++ app/src/main/java/com/spikec/spikec/login.java
@@ -3,11 +3,16 @@ package com.spikec.spikec;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity implements View.OnClickListener {
 Button bottonregister, bottoninicio_sesion;
@@ -29,15 +34,40 @@ Button bottonregister, bottoninicio_sesion;
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Log.i("SESION", "Sesion iniciada "+ user.getEmail());
+                    //si eso es valido poner iniciar una actividad
+                }else{
+                    Log.i("SESION", "sesion cerrada");
+                }
 
-            }
+        }
         };
     }
         private void registrar(String email,String pass){
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass);
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Log.i("SESION", "Usuario creado");
+                    }else {
+                        Log.i("SESION", task.getException().getMessage()+"");
+                    }
+                }
+            });
     }
     private void iniciarsesion(String email,String pass){
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,pass);
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.i("SESION", "Sesion Iniciada");
+                }else {
+                    Log.i("SESION", task.getException().getMessage()+"");
+                }
+            }
+        });;
     }
 
     @Override
@@ -51,8 +81,22 @@ Button bottonregister, bottoninicio_sesion;
             case R.id.btnregister:
                 String emailregistro = editexttcorreo.getText().toString();
                 String passregregistro = editexttcontrasena.getText().toString();
-                iniciarsesion(emailregistro,passregregistro);
+                registrar(emailregistro,passregregistro);
                 break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null ){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
         }
     }
 }
